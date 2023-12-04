b5f98bf642e4bb4ee7da75f957d91a2d14c1cf32
nikiforo <anikiforov@delimobil.ru>
2023-12-04 15:04:10 +0300

    Day4 - make model anemic again

--- src/main/scala/io/github/nikiforo/aoc23/Day4.scala
+++ src/main/scala/io/github/nikiforo/aoc23/Day4.scala
@@ -2,10 +2,7 @@ package io.github.nikiforo.aoc23
 
 object Day4 {
 
-  case class Card(num: Int, wins: List[Int], haves: List[Int]) {
-    val winSet = wins.toSet
-    val winCnt = haves.count(winSet)
-  }
+  case class Card(num: Int, wins: List[Int], haves: List[Int])
 
   def main(args: Array[String]): Unit = {
     val result =
@@ -16,26 +13,31 @@ object Day4 {
 
   def task1(lines: List[String]) =
     lines.map { line =>
-      val card = parse(line)
-      if (card.winCnt == 0) 0 else Array.fill(card.winCnt - 1)(2).product
+      val cnt = winCnt(parse(line))
+      if (cnt == 0) 0 else Array.fill(cnt - 1)(2).product
     }.sum
 
   def task2(lines: List[String]) =
     goTask2(lines.map(l => (1, parse(l))), 0)
 
-  def goTask2(cards: List[(Int, Card)], acc: Int): Int =
+  private def winCnt(card: Card): Int = {
+    val winSet = card.wins.toSet
+    card.haves.count(winSet)
+  }
+
+  private def goTask2(cards: List[(Int, Card)], acc: Int): Int =
     cards match {
       case Nil => acc
       case (amount, card) :: tail =>
-        val (left, right) = tail.splitAt(card.winCnt)
+        val (left, right) = tail.splitAt(winCnt(card))
         goTask2(left.map(pair => (pair._1 + amount, pair._2)) ::: right, acc + amount)
     }
 
   private val CardR = "Card +(\\d+): +(.+) \\| +(.+)".r
 
-  def parse(line: String): Card =
+  private def parse(line: String): Card =
     line match { case CardR(num, wins, haves) => Card(num.toInt, parseInts(wins), parseInts(haves)) }
 
-  def parseInts(ints: String): List[Int] =
+  private def parseInts(ints: String): List[Int] =
     ints.split(" +").map(_.toInt).toList
 }
