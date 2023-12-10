012996f83e06f19d4f71e3ed971c17b0156f2e53
nikiforo <anikiforov@delimobil.ru>
2023-12-10 18:04:26 +0300

    Day10 S case

--- src/main/scala/io/github/nikiforo/aoc23/Day10.scala
+++ src/main/scala/io/github/nikiforo/aoc23/Day10.scala
@@ -2,9 +2,9 @@ package io.github.nikiforo.aoc23
 
 object Day10 {
 
-  type Coord = (Int, Int)
+  private type Coord = (Int, Int)
 
-  final class Scheme(lines: List[String]) {
+  private final class Scheme(lines: List[String]) {
     private val arr = lines.toArray.map(_.toArray)
 
     def indices = arr.indices.map(i => arr(i).indices.map((i, _)))
@@ -18,27 +18,12 @@ object Day10 {
     def get(coord: Coord): Char = find(coord).get
   }
 
-  object Directions {
-    val goUp = (coord: Coord) => (coord._1 - 1, coord._2)
-    val goDown = (coord: Coord) => (coord._1 + 1, coord._2)
-    val goRight = (coord: Coord) => (coord._1, coord._2 + 1)
-    val goLeft = (coord: Coord) => (coord._1, coord._2 - 1)
-
-    def allDirections = List(goUp, goDown, goRight, goLeft)
-
-    def getOuts(char: Char): List[Coord => Coord] = 
-      char match {
-        case ('|') => List(goUp, goDown)
-        case ('-') => List(goLeft, goRight)
-        case ('L') => List(goUp, goRight)
-        case ('J') => List(goLeft, goUp)
-        case ('7') => List(goLeft, goDown)
-        case ('F') => List(goDown, goRight)
-        case _ => List.empty
-      }
-  }
-  
   private val cross = Set('|', 'J', 'L')
+
+  private val goUp = (coord: Coord) => (coord._1 - 1, coord._2)
+  private val goDown = (coord: Coord) => (coord._1 + 1, coord._2)
+  private val goRight = (coord: Coord) => (coord._1, coord._2 + 1)
+  private val goLeft = (coord: Coord) => (coord._1, coord._2 - 1)
 
   def main(args: Array[String]): Unit = {
     val result =
@@ -52,13 +37,15 @@ object Day10 {
 
   def task2(lines: List[String]) = {
     val scheme = new Scheme(lines)
-    val List(route) = buildRoute(scheme).map(_.toSet).distinct
+    val routeList = buildRoute(scheme).head
+    val startNeighbours = Set(routeList.head, routeList.takeRight(2).head)
+    val route = routeList.toSet
     scheme.indices.map { inds =>
       inds.foldLeft((false, 0)) { case ((inside, qty), coord) =>
         val ch = scheme.get(coord)
-        val intersect = cross(ch) || ch == 'S' && route(Directions.goUp(coord))
+        val intersect = cross(ch) || ch == 'S' && startNeighbours(goUp(coord))
         if (route(coord) && intersect) (!inside, qty)
-        else if(route(coord)) (inside, qty)
+        else if (route(coord)) (inside, qty)
         else if (inside) (inside, qty + 1)
         else (inside, qty)
       }._2
@@ -72,11 +59,22 @@ object Day10 {
       else
         for {
           c <- scheme.find(coord)
-          outs = Directions.getOuts(c).map(_.apply(coord))
+          outs = getOuts(c).map(_.apply(coord))
           next <- Option.when(outs.contains(path.head))(outs.find(_ != path.head)).flatten
           evalNext <- go(next, coord :: path)
         } yield evalNext
 
-    Directions.allDirections.map(f => go(f(start), List(start))).flatten
+    List(goUp, goDown, goRight, goLeft).map(f => go(f(start), List(start))).flatten
   }
+
+  private def getOuts(char: Char): List[Coord => Coord] = 
+    char match {
+      case ('|') => List(goUp, goDown)
+      case ('-') => List(goLeft, goRight)
+      case ('L') => List(goUp, goRight)
+      case ('J') => List(goLeft, goUp)
+      case ('7') => List(goLeft, goDown)
+      case ('F') => List(goDown, goRight)
+      case _ => List.empty
+    }
 }
--- src/test/scala/io/github/nikiforo/aoc23/Day10Suite.scala
+++ src/test/scala/io/github/nikiforo/aoc23/Day10Suite.scala
@@ -22,11 +22,27 @@ class Day10Suite extends AnyFunSuite {
       |.L--JOL--J.
       |.....O.....""".stripMargin
 
+  val sample3 =
+    """.F----7F7F7F7F-7....
+      |.|F--7||||||||FJ....
+      |.||.FJ||||||||L7....
+      |FJL7L7LJLJ||LJ.L-7..
+      |L--J.L7...LJS7F-7L7.
+      |....F-J..F7FJ|L7L7L7
+      |....L7.F7||L7|.L7L7|
+      |.....|FJLJ|FJ|F7|.LJ
+      |....FJL-7.||.||||...
+      |....L---J.LJ.LJLJ...""".stripMargin
+
   test("task1") {
     assert(Day10.task1(sample.linesIterator.toList) == 8)
   }
 
   test("task2 sample2") {
     assert(Day10.task2(sample2.linesIterator.toList) == 4)
+  }
+
+  test("task2 sample3") {
+    assert(Day10.task2(sample3.linesIterator.toList) == 8)
   }
 }
