b45095446d1a2dd9ae933f36549e9c7cbd3cb1bc
nikiforo <anikiforov@delimobil.ru>
2023-12-12 21:34:58 +0300

    Day12 task2

--- src/main/scala/io/github/nikiforo/aoc23/Day12.scala
+++ src/main/scala/io/github/nikiforo/aoc23/Day12.scala
@@ -2,6 +2,52 @@ package io.github.nikiforo.aoc23
 
 object Day12 {
 
+  import scala.collection.mutable
+
+  abstract class TopDownTab[S, O](tab: mutable.Map[S, O]) {
+
+    final def compute(s: S): O = 
+      tab.get(s).getOrElse {
+        val result = bare(s)
+        tab(s) = result
+        result
+      }
+
+    protected def bare(s: S): O
+  }
+
+  type Request = (List[Char], Int, List[Int])
+
+  final class Solver(tab: mutable.Map[Request, Long]) extends TopDownTab(tab) {
+
+    def bare(request: Request): Long = {
+      val (chars, continue, next) = request
+      chars match {
+        case Nil => if (continue <= 0 && next.isEmpty) 1 else 0
+        case h :: tail =>
+          def broken =
+            next match {
+              case Nil => 0
+              case i :: iTail => compute(tail, i - 1, iTail)
+            }
+          def cont = compute(tail, continue - 1, next)
+
+          if (continue < 0)
+            if (h == '.') cont
+            else if (h == '#') broken
+            else cont + broken
+          else if (continue == 0)
+            if (h == '.') cont
+            else if (h == '#') 0
+            else cont
+          else
+            if (h == '.') 0
+            else if (h == '#') cont
+            else cont
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val result =
       s"""task1: ${task1(aocLines("12"))}
@@ -12,39 +58,16 @@ object Day12 {
   def task1(lines: List[String]) =
     lines.map { line =>
       val (chars, next) = parse(line)
-      count(chars, -1, next)
+      val solver = new Solver(mutable.Map.empty)
+      solver.compute((chars, -1, next))
     }.sum
 
   def task2(lines: List[String]) =
-    lines.map { line =>
+    lines.zipWithIndex.map { case (line, ind) =>
       val (chars, next) = parse2(line)
-      count(chars, -1, next)
+      val solver = new Solver(mutable.Map.empty)
+      solver.compute((chars, -1, next))
     }.sum
-
-  def count(chars: List[Char], continue: Int, next: List[Int]): Int =
-    chars match {
-      case Nil => if (continue <= 0 && next.isEmpty) 1 else 0
-      case h :: tail =>
-        def broken =
-          next match {
-            case Nil => 0
-            case i :: iTail => count(tail, i - 1, iTail)
-          }
-        def cont = count(tail, continue - 1, next)
-
-        if (continue < 0)
-          if (h == '.') cont
-          else if (h == '#') broken
-          else cont + broken
-        else if (continue == 0)
-          if (h == '.') cont
-          else if (h == '#') 0
-          else cont
-        else
-          if (h == '.') 0
-          else if (h == '#') cont
-          else cont
-    }
 
   def parse(line: String) =
     line match { case s"$record $other" => (record.toList, other.split(",").map(_.toInt).toList) }
--- src/test/scala/io/github/nikiforo/aoc23/Day12Suite.scala
+++ src/test/scala/io/github/nikiforo/aoc23/Day12Suite.scala
@@ -14,15 +14,15 @@ final class Day12Suite extends AnyFunSuite {
 
   val lines = sample.linesIterator.toList
 
-  test("task1 line1") {
-    val (chars, next) = Day12.parse(lines(0))
-    assert(Day12.count(chars, -1, next) == 1)
-  }
+  // test("task1 line1") {
+  //   val (chars, next) = Day12.parse(lines(0))
+  //   assert(Day12.count(chars, -1, next) == 1)
+  // }
 
-  test("task1 line2") {
-    val (chars, next) = Day12.parse(lines(1))
-    assert(Day12.count(chars, -1, next) == 4)
-  }
+  // test("task1 line2") {
+  //   val (chars, next) = Day12.parse(lines(1))
+  //   assert(Day12.count(chars, -1, next) == 4)
+  // }
 
   test("task1") {
     assert(Day12.task1(lines) == 21)
