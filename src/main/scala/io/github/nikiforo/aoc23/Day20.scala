f8d83601315d7c463587842e1bf6b612391fa307
nikiforo <anikiforov@delimobil.ru>
2023-12-20 23:28:20 +0300

    Day20 task2 unsolved

--- src/main/scala/io/github/nikiforo/aoc23/Day20.scala
+++ src/main/scala/io/github/nikiforo/aoc23/Day20.scala
@@ -1,6 +1,7 @@
 package io.github.nikiforo.aoc23
 
 import scala.collection.mutable
+import scala.collection.immutable.Queue
 
 object Day20 extends DayApp("20") {
 
@@ -46,38 +47,69 @@ object Day20 extends DayApp("20") {
 
   private val Broadcaster = "broadcaster"
 
-  // 655382198 - too low
-  // 737679780
   def task1(lines: List[String]): Long = {
-    val broadcast = lines.collectFirst { case s"$Broadcaster -> $destinations" => destinations.split(", ") }.get
+    val (broadcast, map) = parse(lines)
+    initModules(map)
+    val queue = Queue.from(broadcast.map((_, Broadcaster, Low: Pulse)))
+    val (highs, lows) = (1 to 1000).map(_ => run(map, queue, 0, 1)).unzip
+    highs.sum * lows.sum
+  }
+
+  private def initModules(map: Map[String, Module]) =
+    for {
+      (name, module) <- map
+      destination <- module.destinations
+      mod <- map.get(destination)
+    } mod.register(name)
+
+  private def run(map: Map[String, Module], queue: Queue[(String, String, Pulse)], high: Long, low: Long): (Long, Long) =
+    if (queue.isEmpty) (high, low)
+    else {
+      val (dest, source, sentPulse) = queue.head
+      val newEntries =
+        for {
+          module <- map.get(dest).toList
+          pulse <- module.receive(source, sentPulse).toList
+          destination <- module.destinations
+        } yield (destination, dest, pulse)
+      val nHigh = if (sentPulse == High) high + 1 else high
+      val nLow = if (sentPulse == Low) low + 1 else low
+      run(map, queue.tail :++ newEntries, nHigh, nLow)
+    }
+
+    // Won't solve in time
+  def task2(lines: List[String]): Long = {
+    val (broadcast, map) = parse(lines)
+    initModules(map)
+    val queue = Queue.from(broadcast.map((_, Broadcaster, Low: Pulse)))
+    from(1L).find(_ => run2(map, queue)).get
+  }
+
+  private def run2(map: Map[String, Module], queue: Queue[(String, String, Pulse)]): Boolean =
+    queue.headOption.fold(false) { case (dest, source, sentPulse) =>
+      if (dest == "rx" && sentPulse == Low) true
+      else {
+        val newEntries =
+          for {
+            module <- map.get(dest).toList
+            pulse <- module.receive(source, sentPulse).toList
+            destination <- module.destinations
+          } yield (destination, dest, pulse)
+        run2(map, queue.tail :++ newEntries)
+      }
+    }
+
+  def from[T](l: Long): LazyList[Long] = {
+    if (l % 1_000_000 == 0) println(l)
+    l #:: from(l + 1)
+  }
+
+
+  private def parse(lines: List[String]) = {
     val modules = lines.collect {
       case s"%$name -> $destinations" => name -> new FlipFlop(destinations.split(", ").toList)
       case s"&$name -> $destinations" => name -> new Conjunction(destinations.split(", ").toList)
     }
-    val map = modules.toMap
-    for {
-      (name, module) <- modules
-      destination <- module.destinations
-      mod <- map.get(destination)
-    } mod.register(name)
-    val (highs, lows) = (1 to 1000).map(_ => run(map, broadcast)).unzip
-    highs.sum * lows.sum
+    (lines.collectFirst { case s"$Broadcaster -> $destinations" => destinations.split(", ") }.get, modules.toMap)
   }
-
-  private def run(map: Map[String, Module], broadcast: Array[String]): (Long, Long) = {
-    var (high, low) = (0L, 1L)
-    val queue = mutable.Queue.from(broadcast.map((_, Broadcaster, Low: Pulse)))
-    while (queue.nonEmpty) {
-      val (dest, source, sentPulse) = queue.dequeue()
-      if (sentPulse == High) high += 1 else low += 1
-      for {
-        module <- map.get(dest)
-        pulse <- module.receive(source, sentPulse)
-        destination <- module.destinations
-      } queue.enqueue((destination, dest, pulse))
-    }
-    (high, low)
-  }
-
-  def task2(lines: List[String]): Long = unsolved
 }
