81ebbb5e5a6ae3d3ad497f5b0f2fba1519c2c721
nikiforo <anikiforov@delimobil.ru>
2023-12-23 13:34:53 +0300

    mark timouted

--- src/test/scala/io/github/nikiforo/aoc23/Day15Suite.scala
+++ src/test/scala/io/github/nikiforo/aoc23/Day15Suite.scala
@@ -1,7 +1,9 @@
 package io.github.nikiforo.aoc23
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.DoNotDiscover
 
+@DoNotDiscover
 class Day15Suite extends AnyFunSuite {
   val initSeq = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
--- src/test/scala/io/github/nikiforo/aoc23/Day18Suite.scala
+++ src/test/scala/io/github/nikiforo/aoc23/Day18Suite.scala
@@ -1,7 +1,9 @@
 package io.github.nikiforo.aoc23
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.DoNotDiscover
 
+@DoNotDiscover
 final class Day18Suite extends AnyFunSuite {
 
   val sample =
